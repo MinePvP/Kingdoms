8aef092c5e531c0a780e931eb3639221c28a4077
Andreas Reinhold / reini <reini@nonameweb.ch>
2012-11-25 09:51:29 +0100

    create selection whitout getOwner() then Commponant is Attached on a Player

--- src/main/java/ch/minepvp/spout/kingdoms/component/SelectionComponent.java
+++ src/main/java/ch/minepvp/spout/kingdoms/component/SelectionComponent.java
@@ -2,7 +2,6 @@ package ch.minepvp.spout.kingdoms.component;
 
 import ch.minepvp.spout.kingdoms.protection.Selection;
 import org.spout.api.component.components.EntityComponent;
-import org.spout.api.entity.Player;
 
 public class SelectionComponent extends EntityComponent {
 
@@ -10,7 +9,7 @@ public class SelectionComponent extends EntityComponent {
 
     @Override
     public void onAttached() {
-        selection = new Selection( (Player)getOwner() );
+        selection = new Selection();
     }
 
     public Selection getSelection() {
