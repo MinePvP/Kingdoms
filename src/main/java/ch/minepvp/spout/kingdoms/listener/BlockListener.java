503bc82f0e58b93683d6fac216cdd245ce3ed94b
Andreas Reinhold / reini <reini@nonameweb.ch>
2012-11-26 20:39:14 +0100

    add protection stuff

--- src/main/java/ch/minepvp/spout/kingdoms/listener/BlockListener.java
+++ src/main/java/ch/minepvp/spout/kingdoms/listener/BlockListener.java
@@ -2,13 +2,22 @@ package ch.minepvp.spout.kingdoms.listener;
 
 import ch.minepvp.spout.kingdoms.Kingdoms;
 import ch.minepvp.spout.kingdoms.component.KingdomsComponent;
+import ch.minepvp.spout.kingdoms.database.table.Kingdom;
+import ch.minepvp.spout.kingdoms.database.table.Plot;
+import ch.minepvp.spout.kingdoms.database.table.Zone;
+import ch.minepvp.spout.kingdoms.entity.KingdomRank;
+import ch.minepvp.spout.kingdoms.manager.KingdomManager;
+import ch.minepvp.spout.kingdoms.manager.PlotManager;
+import ch.minepvp.spout.kingdoms.manager.ZoneManager;
 import com.alta189.simplesave.Database;
+import org.spout.api.chat.ChatArguments;
 import org.spout.api.entity.Player;
 import org.spout.api.event.Cause;
 import org.spout.api.event.EventHandler;
 import org.spout.api.event.Listener;
 import org.spout.api.event.Order;
 import org.spout.api.event.block.BlockChangeEvent;
+import org.spout.api.lang.Translation;
 import org.spout.vanilla.event.cause.PlayerBreakCause;
 import org.spout.vanilla.event.cause.PlayerPlacementCause;
 
@@ -17,11 +26,19 @@ public class BlockListener implements Listener {
     private Kingdoms plugin;
     private Database db;
 
+    private KingdomManager kingdomManager;
+    private PlotManager plotManager;
+    private ZoneManager zoneManager;
+
 
     public BlockListener() {
 
         plugin = Kingdoms.getInstance();
         db = plugin.getDatabase();
+
+        kingdomManager = plugin.getKingdomManager();
+        plotManager = plugin.getPlotManager();
+        zoneManager = plugin.getZoneManager();
 
     }
 
@@ -30,19 +47,101 @@ public class BlockListener implements Listener {
 
         Player player;
 
-        // Stats
+        Kingdom kingdom = kingdomManager.getKingdomByPoint( event.getBlock().getPosition() );
+        Plot plot = plotManager.getPlotByPoint( event.getBlock().getPosition() );
+        Zone zone = zoneManager.getZoneByPoint( event.getBlock().getPosition() );
+
+        if ( kingdom == null && plot == null && zone == null ) {
+            return;
+        }
+
         if ( event.getCause() instanceof PlayerBreakCause ) {
 
             player = ((PlayerBreakCause) event.getCause()).getSource();
 
+            if ( kingdom != null ) {
 
+                if ( plot != null ) {
 
+                    // Plot
+                    if ( !plot.getOwner().equalsIgnoreCase( player.getName() ) ) {
+                        event.setCancelled(true);
+                        player.sendMessage( ChatArguments.fromFormatString(Translation.tr("{{RED}}You cant Build in a others Player Plot!", player) ) );
+                        return;
+                    }
+
+                } else {
+
+                    // Kingdom
+                    if ( kingdom.getId() != player.get(KingdomsComponent.class).getKingdom().getId() ) {
+                        event.setCancelled(true);
+                        player.sendMessage( ChatArguments.fromFormatString(Translation.tr("{{RED}}You cant Build in a other Kingdom!", player) ) );
+                        return;
+                    }
+
+                    if ( player.get(KingdomsComponent.class).getMember().getRank().equals( KingdomRank.NOVICE ) ) {
+                        event.setCancelled(true);
+                        player.sendMessage( ChatArguments.fromFormatString(Translation.tr("{{RED}}You need to be Member in the Kingdom to Build!", player) ) );
+                        return;
+                    }
+
+                }
+
+            }
+
+            if ( zone!= null ) {
+
+                if ( zone.isBuild() == false ) {
+                    event.setCancelled(true);
+                    player.sendMessage( ChatArguments.fromFormatString(Translation.tr("{{RED}}You cant Build in this Zone!", player) ) );
+                    return;
+                }
+
+            }
 
         } else if ( event.getCause() instanceof PlayerPlacementCause ) {
 
             player = ((PlayerPlacementCause) event.getCause()).getSource();
 
+            if ( kingdom != null ) {
 
+                if ( plot != null ) {
+
+                    // Plot
+                    if ( !plot.getOwner().equalsIgnoreCase( player.getName() ) ) {
+                        event.setCancelled(true);
+                        player.sendMessage( ChatArguments.fromFormatString(Translation.tr("{{RED}}You cant Build in a others Player Plot!", player) ) );
+                        return;
+                    }
+
+                } else {
+
+                    // Kingdom
+                    if ( kingdom.getId() != player.get(KingdomsComponent.class).getKingdom().getId() ) {
+                        event.setCancelled(true);
+                        player.sendMessage( ChatArguments.fromFormatString(Translation.tr("{{RED}}You cant Build in a other Kingdom!", player) ) );
+                        return;
+                    }
+
+                    if ( player.get(KingdomsComponent.class).getMember().getRank().equals( KingdomRank.NOVICE ) ) {
+                        event.setCancelled(true);
+                        player.sendMessage( ChatArguments.fromFormatString(Translation.tr("{{RED}}You need to be Member in the Kingdom to Build!", player) ) );
+                        return;
+                    }
+
+                }
+
+            }
+
+            if ( zone!= null ) {
+
+                if ( zone.isBuild() == false ) {
+                    event.setCancelled(true);
+                    player.sendMessage( ChatArguments.fromFormatString(Translation.tr("{{RED}}You cant Build in this Zone!", player) ) );
+                    return;
+                }
+
+            }
 
         }
 
--- src/main/java/ch/minepvp/spout/kingdoms/listener/PlayerListener.java
+++ src/main/java/ch/minepvp/spout/kingdoms/listener/PlayerListener.java
@@ -6,11 +6,15 @@ import ch.minepvp.spout.kingdoms.component.SelectionComponent;
 import ch.minepvp.spout.kingdoms.config.KingdomsConfig;
 import ch.minepvp.spout.kingdoms.database.table.Kingdom;
 import ch.minepvp.spout.kingdoms.database.table.Member;
+import ch.minepvp.spout.kingdoms.database.table.Plot;
+import ch.minepvp.spout.kingdoms.database.table.Zone;
 import ch.minepvp.spout.kingdoms.entity.KingdomChannel;
+import ch.minepvp.spout.kingdoms.entity.KingdomRank;
+import ch.minepvp.spout.kingdoms.manager.KingdomManager;
 import ch.minepvp.spout.kingdoms.manager.MemberManager;
+import ch.minepvp.spout.kingdoms.manager.PlotManager;
+import ch.minepvp.spout.kingdoms.manager.ZoneManager;
 import org.spout.api.chat.ChatArguments;
-import org.spout.api.chat.channel.ChatChannel;
-import org.spout.api.chat.style.ChatStyle;
 import org.spout.api.entity.Player;
 import org.spout.api.event.EventHandler;
 import org.spout.api.event.Listener;
@@ -19,9 +23,9 @@ import org.spout.api.event.player.PlayerChatEvent;
 import org.spout.api.event.player.PlayerInteractEvent;
 import org.spout.api.event.player.PlayerJoinEvent;
 import org.spout.api.event.player.PlayerLeaveEvent;
+import org.spout.api.geo.cuboid.Block;
 import org.spout.api.lang.Translation;
 import org.spout.vanilla.component.living.Hostile;
-import org.spout.vanilla.component.living.neutral.Human;
 import org.spout.vanilla.component.misc.HealthComponent;
 import org.spout.vanilla.event.player.PlayerDeathEvent;
 import org.spout.vanilla.material.VanillaMaterials;
@@ -30,16 +34,22 @@ public class PlayerListener implements Listener {
 
     private Kingdoms plugin;
     private MemberManager memberManager;
+    private KingdomManager kingdomManager;
+    private PlotManager plotManager;
+    private ZoneManager zoneManager;
 
 
     public PlayerListener() {
 
         plugin = Kingdoms.getInstance();
         memberManager = plugin.getMemberManager();
+        kingdomManager = plugin.getKingdomManager();
+        plotManager = plugin.getPlotManager();
+        zoneManager = plugin.getZoneManager();
 
     }
 
-    @EventHandler (order = Order.MONITOR)
+    @EventHandler(order = Order.MONITOR)
     public void onPlayerJoin( PlayerJoinEvent event ) {
 
         Player player = event.getPlayer();
@@ -55,17 +65,15 @@ public class PlayerListener implements Listener {
 
     @EventHandler (order = Order.MONITOR)
     public void onPlayerQuitEvent( PlayerLeaveEvent event ) {
-
         memberManager.save(event.getPlayer());
-
     }
 
     @EventHandler (order = Order.MONITOR)
     public void onPLayerChatEvent( PlayerChatEvent event) {
 
         Player player = event.getPlayer();
-        Member member = player.add(KingdomsComponent.class).getMember();
-        Kingdom kingdom = player.add(KingdomsComponent.class).getKingdom();
+        Member member = player.get(KingdomsComponent.class).getMember();
+        Kingdom kingdom = player.get(KingdomsComponent.class).getKingdom();
 
         if ( member.getChannel().equals( KingdomChannel.LOCAL ) ) {
 
@@ -79,6 +87,13 @@ public class PlayerListener implements Listener {
 
         } else if ( member.getChannel().equals( KingdomChannel.KINGDOM ) ) {
 
+            for ( Member toMember : kingdom.getOnlineMembers() ) {
+
+                Player toPlayer = plugin.getServer().getPlayer(toMember.getName(), true);
+
+                toPlayer.sendMessage( ChatArguments.fromFormatString("{{BLUE}}[{{GOLD}}KINGDOM{{BLUE}}] {{WHITE}}" + player.getName() + " {{GOLD}}: {{WHITE}}" + event.getMessage().getPlainString()) );
+
+            }
 
 
         } else if ( member.getChannel().equals( KingdomChannel.GLOBAL ) ) {
@@ -108,7 +123,93 @@ public class PlayerListener implements Listener {
     @EventHandler (order = Order.MONITOR)
     public void onPlayerInteractEvent( PlayerInteractEvent event) {
 
+        if ( event.isAir() || event.isCancelled() ) {
+            return;
+        }
+
         Player player = event.getPlayer();
+        Block block = player.getWorld().getBlock( event.getInteractedPoint() );
+
+        if ( block.isMaterial( VanillaMaterials.CHEST,
+                               VanillaMaterials.FURNACE,
+                               VanillaMaterials.FURNACE_BURNING,
+                               VanillaMaterials.BED,
+                               VanillaMaterials.BOOK,
+                               VanillaMaterials.BREWING_STAND,
+                               VanillaMaterials.ENCHANTMENT_TABLE,
+                               VanillaMaterials.JUKEBOX,
+                               VanillaMaterials.LEVER,
+                               VanillaMaterials.STONE_BUTTON,
+                               VanillaMaterials.WOOD_BUTTON) ) {
+
+            Kingdom kingdom = kingdomManager.getKingdomByPoint( event.getInteractedPoint() );
+            Plot plot = plotManager.getPlotByPoint( event.getInteractedPoint() );
+            Zone zone = zoneManager.getZoneByPoint( event.getInteractedPoint() );
+
+            if ( kingdom == null && plot == null && zone == null ) {
+                return;
+            }
+
+            if ( kingdom != null ) {
+
+                if ( plot != null ) {
+
+                    // Plot
+                    if ( !plot.getOwner().equalsIgnoreCase( player.getName() ) ) {
+                        event.setCancelled(true);
+                        player.sendMessage( ChatArguments.fromFormatString(Translation.tr("{{RED}}You cant do this!", player) ) );
+                        return;
+                    }
+
+                } else {
+
+                    // Kingdom
+                    if ( kingdom.getId() != player.get(KingdomsComponent.class).getKingdom().getId() ) {
+                        event.setCancelled(true);
+                        player.sendMessage( ChatArguments.fromFormatString(Translation.tr("{{RED}}You cant do this!", player) ) );
+                        return;
+                    }
+
+                    if ( player.get(KingdomsComponent.class).getMember().getRank().equals( KingdomRank.NOVICE ) ) {
+                        event.setCancelled(true);
+                        player.sendMessage( ChatArguments.fromFormatString(Translation.tr("{{RED}}You cant do this!", player) ) );
+                        return;
+                    }
+
+                }
+
+            }
+
+            if ( zone!= null ) {
+
+                if ( zone.getKingdom() != null ) {
+
+                    if ( zone.getKingdom().getId() != player.get(KingdomsComponent.class).getKingdom().getId() ) {
+                        event.setCancelled(true);
+                        player.sendMessage( ChatArguments.fromFormatString(Translation.tr("{{RED}}You cant do this!", player) ) );
+                        return;
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+    @EventHandler (order = Order.MONITOR)
+    public void onPlayerInteractEventSelection( PlayerInteractEvent event) {
+
+
+        if ( event.isAir() || event.isCancelled() ) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+
+        player.sendMessage("Test");
 
         if ( player.hasPermission("kingdoms.zones.selection") ) {
 
@@ -119,12 +220,12 @@ public class PlayerListener implements Listener {
                     switch ( event.getAction() ) {
                         case LEFT_CLICK:
                             player.get(SelectionComponent.class).getSelection().setPoint1( event.getInteractedPoint() );
-                            player.sendMessage(ChatArguments.fromFormatString(Translation.tr("Point 1 selected!", player)) );
+                            player.sendMessage(ChatArguments.fromFormatString(Translation.tr("Point 1 : %0, X : %1  Y : %2 Z : %3", player,
+                                    event.getInteractedPoint().getWorld().getName(), event.getInteractedPoint().getX(), event.getInteractedPoint().getY(), event.getInteractedPoint().getZ())) );
                         case RIGHT_CLICK:
                             player.get(SelectionComponent.class).getSelection().setPoint2(event.getInteractedPoint() );
-                            player.sendMessage(ChatArguments.fromFormatString(Translation.tr("Point 2 selected!", player) ) );
-                        default:
-                            return;
+                            player.sendMessage(ChatArguments.fromFormatString(Translation.tr("Point 2 : %0, X : %1  Y : %2 Z : %3", player,
+                                    event.getInteractedPoint().getWorld().getName(), event.getInteractedPoint().getX(), event.getInteractedPoint().getY(), event.getInteractedPoint().getZ())) );
                     }
 
                 }
@@ -155,7 +256,6 @@ public class PlayerListener implements Listener {
             if ( killer.get(KingdomsComponent.class).getKingdom() != null ) {
                 killer.get(KingdomsComponent.class).getKingdom().addPlayerKill();
             }
-
 
         } else if ( player.get(HealthComponent.class).getLastDamager() instanceof Hostile) {
 
